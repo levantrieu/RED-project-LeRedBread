edd9e2355c8c16010590916bb14ae71d8e645776
Levan <user@Macintosh.local>
2016-03-13 20:56:31 -0700

    Add widget to footer

--- wp-content/themes/LeRedBread/footer.php
+++ wp-content/themes/LeRedBread/footer.php
@@ -1,33 +1,41 @@
 <?php
 /**
- * The template for displaying the footer.
- *
- * @package RED_Starter_Theme
- */
+* The template for displaying the footer.
+*
+* @package RED_Starter_Theme
+*/
 
 ?>
 
-			</div><!-- #content -->
+</div><!-- #content -->
 
-			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-container footer-contact">
-					<div class="contact-info">
-						<p class="contact-header">Contact</p>
-						<p><i class="fa fa-envelope"></i> info@leredbread.com</p>
-						<p><i class="fa fa-phone"></i> (555) 343 456 7891</p>
-						<p class="social-icons-footer">
-							<a href="#" target="blank"><i class="fa fa-twitter-square"></i></a>
-							<a href="#" target="blank"><i class="fa fa-facebook-square"></i></a>
-							<a href="#" target="blank"><i class="fa fa-google-plus-square"></i></a>
-						</p>
-					</div>
-					<div class="logo-footer"><img src="<?php bloginfo( 'template_directory' ); ?>/images/lrb-logo-white.svg" /></div>
-					<div>Business hours</div>
-				</div>
-				<div class="copyright">Copyright &copy; 2016 Le Red Bread</div>
-			</footer><!-- #colophon -->
-		</div><!-- #page -->
+<footer id="colophon" class="site-footer" role="contentinfo">
+	<div class="site-container footer-contact">
+		<div class="contact-info">
+			<p class="contact-header">Contact</p>
+			<p><i class="fa fa-envelope"></i> info@leredbread.com</p>
+			<p><i class="fa fa-phone"></i> (555) 343 456 7891</p>
+			<p class="social-icons-footer">
+				<a href="#" target="blank"><i class="fa fa-twitter-square"></i></a>
+				<a href="#" target="blank"><i class="fa fa-facebook-square"></i></a>
+				<a href="#" target="blank"><i class="fa fa-google-plus-square"></i></a>
+			</p>
+		</div>
+		<div class="logo-footer"><img src="<?php bloginfo( 'template_directory' ); ?>/images/lrb-logo-white.svg" /></div>
+		<div id="footer-sidebar" class="secondary">
+			<div id="footer-sidebar1">
+				<?php
+				if(is_active_sidebar('footer-sidebar-1')){
+					dynamic_sidebar('footer-sidebar-1');
+				}
+				?>
+			</div>
+		</div>
+	</div>
+	<div class="copyright">Copyright &copy; 2016 Le Red Bread</div>
+</footer><!-- #colophon -->
+</div><!-- #page -->
 
-		<?php wp_footer(); ?>
-	</body>
+<?php wp_footer(); ?>
+</body>
 </html>
